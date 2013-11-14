20ae7f617a674944512e7686ea5d4c6813c941c2
0xD34D <clark@scheffsblend.com>
2013-11-13 18:17:15 -0800

    Add native java screen recorder [1/2]
    This is based on the screenrecorder binary found in frameworks/av.
    It has been converted to a native library with the necessary jni
    hooks for interfacing withit via a ScreenRecorder java class.
    
    Volume up + power chord is used to start and stop recording by
    binding to the service found in the ScreenRecorder app:
    https://github.com/ChameleonOS/android_packages_apps_ScreenRecorder
    
    PS1:
    Clean up white space.
    
    PS2:
    Fix naming convention for state field.
    
    PS3:
    Rebase
    
    PS4:
    Post mScreenRecordTimeout with 1000 millisecond delay instead
    of 10000.
    
    PS5:
    Send record screen broadcast instead of using the service
    directly.
    
    PS6:
    SCREEN_RECORDER_FRAMERATE should be SCREEN_RECORDER_BITRATE
    
    Conflicts:
    
    	core/java/android/provider/Settings.java
    
    Change-Id: I5b7af9b4b4f24a8c54ac0f9dcb36ace16672e2cf
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	core/res/res/values/strings.xml
    	policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
    
    Conflicts:
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2797,6 +2797,18 @@ public final class Settings {
         public static final int STATUS_BAR_COLLAPSE_IF_NO_CLEARABLE = 2;
 
         /**
+         * Width and height of output vide expressed in WxH
+         * @hide
+         */
+        public static final String SCREEN_RECORDER_OUTPUT_DIMENSIONS = "screen_recorder_output_dimensions";
+
+        /**
+         * Screen recorder framerate in bits per second
+         * @hide
+         */
+        public static final String SCREEN_RECORDER_BITRATE = "screen_recorder_bitrate";
+
+        /**
         * Whether to control brightness from status bar
         *
         * @hide
