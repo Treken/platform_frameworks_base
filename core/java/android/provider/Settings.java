6c283e76d42b449b873e4a56bcfd6a46a2c8f9be
Lars Greiss <kufikugel@googlemail.com>
2013-12-01 06:03:10 +0100

    fb: notification transparency (1/2)
    this add back notification transparency...compared to 4.3
    it controlls as well the notification icon backgound transparency
    which felt in the past dissplaced. As well codecleanup of the old
    implementation.
    
    largeIcon support I did not add due that google does not support it
    as well at current status...it is only a placeholder in
    NotificationData
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
    
    Change-Id: Ic5bb8998f64cac573d70005fad909104accc9e96

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2851,6 +2851,12 @@ public final class Settings {
         */
         public static final String NOTIFICATION_BACKGROUND_ALPHA = "notification_background_alpha";
 
+       /**
+        * Sets the alpha (transparency) of the notification
+        * @hide
+        */
+        public static final String NOTIFICATION_ALPHA = "notification_alpha";
+
         /**
          * Stores the value for notification shortcuts icon color
          * @hide
