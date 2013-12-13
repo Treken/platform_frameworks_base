5c31438ea74edb1a893c1d0dda0ab20e9bd8cf0d
cphelps76 <cphelps76@gmail.com>
2013-12-12 21:35:01 -0600

    Frameworks: Port BatteryBar (1/2) {x} Adds Battery bar with following features: {x} location set {x} color {x} aligment {x} thickness {x} charging animation
    Change-Id: Ie9a168121dc569ec3af068a4bda6b46a4e896b2d
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	packages/SystemUI/res/values/attrs.xml
    	packages/SystemUI/src/com/android/systemui/statusbar/policy/Prefs.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2664,6 +2664,32 @@ public final class Settings {
         public static final String STATUSBAR_CLOCK_AM_PM_STYLE = "statusbar_clock_am_pm_style";
 
         /**
+         * Whether to show the battery bar
+         * @hide
+         */
+        public static final String STATUSBAR_BATTERY_BAR = "statusbar_battery_bar";
+
+        /**
+         * @hide
+         */
+        public static final String STATUSBAR_BATTERY_BAR_COLOR = "statusbar_battery_bar_color";
+
+        /**
+         * @hide
+         */
+        public static final String STATUSBAR_BATTERY_BAR_THICKNESS = "statusbar_battery_bar_thickness";
+
+        /**
+         * @hide
+         */
+        public static final String STATUSBAR_BATTERY_BAR_STYLE = "statusbar_battery_bar_style";
+
+        /**
+         * @hide
+         */
+        public static final String STATUSBAR_BATTERY_BAR_ANIMATE = "statusbar_battery_bar_animate";
+
+         /**
          * Style of clock
          * 0 - Hide Clock
          * 1 - Right Clock
