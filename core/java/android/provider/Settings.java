de66017b6689d2b9387a8aa474dc12972d5ce18c
cphelps76 <cphelps76@gmail.com>
2013-12-12 21:35:01 -0600

    Frameworks: Port BatteryBar (1/2) {x} Adds Battery bar with following features: {x} location set {x} color {x} aligment {x} thickness {x} charging animation
    Change-Id: Ie9a168121dc569ec3af068a4bda6b46a4e896b2d
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	packages/SystemUI/res/values/attrs.xml
    	packages/SystemUI/src/com/android/systemui/statusbar/policy/Prefs.java
    
    Conflicts:
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2590,6 +2590,32 @@ public final class Settings {
          */
         public static final String REVERSE_LOOKUP_PROVIDER = "reverse_lookup_provider";
 
+        /**        
+         * Whether to show the battery bar
+         * @hide
+         */
+        public static final String STATUSBAR_BATTERY_BAR = "statusbar_battery_bar";
+
+        /**
+         * @hide
+         */
+        public static final String STATUSBAR_BATTERY_BAR_COLOR = "statusbar_battery_bar_color";
+
+        /**
+         * @hide
+         */
+        public static final String STATUSBAR_BATTERY_BAR_THICKNESS = "statusbar_battery_bar_thickness";
+
+        /**
+         * @hide
+         */
+        public static final String STATUSBAR_BATTERY_BAR_STYLE = "statusbar_battery_bar_style";
+
+        /**
+         * @hide
+         */
+        public static final String STATUSBAR_BATTERY_BAR_ANIMATE = "statusbar_battery_bar_animate";
+
         /**
          * I am the lolrus.
          * <p>
