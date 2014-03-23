VanirAOSP/frameworks_base

Add Music Tile (1/2)
Tap to play/pause. Longpress for next track

Author: kufikugel

Change-Id: I7bde0969adc2de15fbf8c3243d0d216006cf363a

packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettingsController.java
@@ -29,6 +29,7 @@ import static com.android.internal.util.cm.QSConstants.TILE_GPS;
 import static com.android.internal.util.cm.QSConstants.TILE_LOCKSCREEN;
 import static com.android.internal.util.cm.QSConstants.TILE_LTE;
 import static com.android.internal.util.cm.QSConstants.TILE_MOBILEDATA;
+import static com.android.internal.util.cm.QSConstants.TILE_MUSIC;
 import static com.android.internal.util.cm.QSConstants.TILE_NETWORKADB;
 import static com.android.internal.util.cm.QSConstants.TILE_NETWORKMODE;
 import static com.android.internal.util.cm.QSConstants.TILE_NFC;
@@ -78,6 +79,7 @@ import com.android.systemui.quicksettings.InputMethodTile;
 import com.android.systemui.quicksettings.LteTile;
 import com.android.systemui.quicksettings.MobileNetworkTile;
 import com.android.systemui.quicksettings.MobileNetworkTypeTile;
+import com.android.systemui.quicksettings.MusicTile;
 import com.android.systemui.quicksettings.NetworkAdbTile;
 import com.android.systemui.quicksettings.NfcTile;
 import com.android.systemui.quicksettings.PerformanceProfileTile;
@@ -270,6 +272,8 @@ public class QuickSettingsController {
                 if (QSUtils.expandedDesktopEnabled(resolver)) {
                     qs = new ExpandedDesktopTile(mContext, this, mHandler);
                 }*/
+            } else if (tile.equals(TILE_MUSIC)) {
+                qs = new MusicTile(mContext, this);
             } else if (tile.equals(TILE_NETWORKADB)) {
                 mTileStatusUris.add(Settings.Global.getUriFor(Settings.Global.ADB_ENABLED));
                 if (QSUtils.adbEnabled(resolver)) {

Treken/platform_frameworks_base

Conflicts:
	packages/SystemUI/src/com/android/systemui/quicksettings/MusicTile.java
	packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettingsController.java

packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettingsController.java
@@ -28,6 +28,7 @@ import static com.android.internal.util.oct.QSConstants.TILE_GPS;
 import static com.android.internal.util.oct.QSConstants.TILE_LOCKSCREEN;
 import static com.android.internal.util.oct.QSConstants.TILE_LTE;
 import static com.android.internal.util.oct.QSConstants.TILE_MOBILEDATA;
+import static com.android.internal.util.oct.QSConstants.TILE_MUSIC;
 import static com.android.internal.util.oct.QSConstants.TILE_NETWORKMODE;
 import static com.android.internal.util.oct.QSConstants.TILE_NFC;
 import static com.android.internal.util.oct.QSConstants.TILE_RINGER;
@@ -71,6 +72,7 @@ import com.android.systemui.quicksettings.InputMethodTile;
 import com.android.systemui.quicksettings.LteTile;
 import com.android.systemui.quicksettings.MobileNetworkTile;
 import com.android.systemui.quicksettings.MobileNetworkTypeTile;
+import com.android.systemui.quicksettings.MusicTile;
 import com.android.systemui.quicksettings.NfcTile;
 import com.android.systemui.quicksettings.PreferencesTile;
 import com.android.systemui.quicksettings.QuickSettingsTile;
@@ -239,6 +241,8 @@ public class QuickSettingsController {
                 qs = new LteTile(mContext, this);
             } else if (tile.equals(TILE_VOLUME)) {
                 qs = new VolumeTile(mContext, this, mHandler);
+            } else if (tile.equals(TILE_MUSIC)) {
+                qs = new MusicTile(mContext, this);
             }
 
             if (qs != null) {