a6160e7bcc8211e679119db09f9bc0a549265665
Andy Mast <andy@cyngn.com>
2014-05-01 10:09:35 -0700

    Theme engine: Support launcher based icon packs
    Before this patch, icon packs written for Trebuchet/Nova/Apex could not be used
    with the new theme engine as the formats were different. Particularly the icon package IDs are 7F,
    the same ID as as regular apps. Furthermore the location of resources in these icon packs was different
    from what the theme engine expects. This patch adds support for these icon packs by overriding
    the pkg id.
    
    AssetManager now can override package ids without requiring aapt to recompile the APK.
    Note: XML references will still use the original id compiled with AAPT, so it is only useful for resources
    which do not use xml drawables (such as icon packs).
    
    PackageInfo now has a new member identify "legacy" (3rd party icon packs) written
    for launcher's like Trebuchet, Nova, and Apex.
    
    Change-Id: Icd12c097e156c9626df0772abeb50e00f7b61f64
    
    Conflicts:
    	core/java/android/content/res/AssetManager.java
    	include/androidfw/AssetManager.h

--- core/java/android/content/res/AssetManager.java
+++ core/java/android/content/res/AssetManager.java
@@ -641,7 +641,9 @@ public final class AssetManager {
                                            String targetPkgPath, String prefixPath);
 
     /**
-     * Add a set of assets as an icon pack.
+     * Add a set of assets as an icon pack. A pkgIdOverride value will change the package's id from
+     * what is in the resource table to a new value. Manage this carefully, if icon pack has more
+     * than one package then that next package's id will use pkgIdOverride+1.
      *
      * Icon packs are different from overlays as they have a different pkg id and
      * do not use idmap so no targetPkg is required
@@ -649,15 +651,7 @@ public final class AssetManager {
      * {@hide}
      */
     public native final int addIconPath(String idmapPath, String resArscPath, String resApkPath,
-                                        String prefixPath);
-
-    /**
-     * Add a set of common assets.
-     *
-     * {@hide}
-     */
-    public native final int addCommonOverlayPath(String idmapPath, String resArscPath,
-                                                 String resApkPath, String prefixPath);
+                                        String prefixPath, int pkgIdOverride);
 
     /**
     * Delete a set of overlay assets from the asset manager. Not for use by
